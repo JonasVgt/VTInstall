d7a2af3206e6dfe63cb42649dd333e7a56444558
JonasVgt <git@jonasvoigt.de>
2022-12-17 20:31:34 +0100

    added basic CLI

--- vtinstall/src/main.rs
+++ vtinstall/src/main.rs
@@ -1,4 +1,16 @@
+use clap::Parser;
+
+///Tool for creating install scripts
+#[derive(Parser, Debug)]
+#[command(author,version, about)]
+struct Args {
+   /// Path of the install script   
+   #[arg(value_name="path")]
+   path: String,
+
+
+}
 
 fn main() {
-
+   let args = Args::parse();
 }
