b79e502d549585a5f86c7ae9ff5c8d0aedb70c91
JonasVgt <git@jonasvoigt.de>
2022-12-19 10:51:10 +0100

    compiler now writes correctly

--- vtinstall/parser/src/instruction.rs
+++ vtinstall/parser/src/instruction.rs
@@ -1,37 +1,51 @@
-use std::{process::{Command, Stdio}, io::Write, fmt::Debug};
+use std::{
+    fmt::Debug,
+    io::Write,
+    process::{Command, Stdio},
+};
 
 use regex::internal::Inst;
 
-pub struct Instruction{
+pub struct Instruction {
     name: String,
     executable: String,
-    dry_run_executable: String
+    dry_run_executable: String,
 }
 
-
 impl Instruction {
-    
-    pub fn get_run_instruction() -> Instruction{
-        Instruction { name: String::from("RUN"), executable: String::from("$*"), dry_run_executable: String::from("echo hello") }
+    pub fn get_run_instruction() -> Instruction {
+        Instruction {
+            name: String::from("RUN"),
+            executable: String::from("$*;"),
+            dry_run_executable: String::from("echo hello;"),
+        }
     }
 
-
-    pub fn execute(&self, dry_run : bool, args: &Vec<String>){
-        let exec = if dry_run {&self.dry_run_executable} else {&self.executable};
+    pub fn execute(&self, dry_run: bool, args: &Vec<String>) {
+        let exec = if dry_run {
+            &self.dry_run_executable
+        } else {
+            &self.executable
+        };
 
         let mut cmd = Command::new(exec).args(args).spawn().unwrap();
         cmd.wait().unwrap();
     }
 
-    pub fn to_bash(&self, dry_run : bool) -> &str{
-        if dry_run {self.dry_run_executable.as_str()} else {self.executable.as_str()}
+    pub fn to_bash(&self, dry_run: bool) -> &str {
+        if dry_run {
+            self.dry_run_executable.as_str()
+        } else {
+            self.executable.as_str()
+        }
     }
-
 }
 
 impl Debug for Instruction {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("Instruction").field("name", &self.name).finish()
+        f.debug_struct("Instruction")
+            .field("name", &self.name)
+            .finish()
     }
 }
 
--- vtinstall/src/compiler/mod.rs
+++ vtinstall/src/compiler/mod.rs
@@ -1,6 +1,6 @@
 mod errors;
 
-use std::{fs, os, path::Path};
+use std::{fs::{self, File}, path::Path, io::Write};
 
 use parser::script::Script;
 
@@ -23,18 +23,29 @@ pub fn compile(script: Script, target: &Path, dry_run: bool) -> Result<(), Compi
 }
 
 fn compile_script(script: Script, root: &Path, dry_run: bool) -> Result<(), CompileError> {
-    let target = root.join(script.name().to_owned() + ".sh");
+    let target_path = root.join(script.name().to_owned() + ".sh");
 
-    if target.exists() {
-        return Err(CompileError::script_duplicate_name_error(format!("File at: {} already exists!", target.to_str().unwrap_or(""))));
+    if target_path.exists() {
+        return Err(CompileError::script_duplicate_name_error(format!("File at: {} already exists!", target_path.to_str().unwrap_or(""))));
     }
 
-    if let Err(error) = fs::write(&target, "#!/bin/bash"){
+    let mut file = match File::create(target_path){
+        Ok(f) => f,
+        Err(error) => return Err(CompileError::io_error(error.to_string()))
+    };
+
+    if let Err(error) = file.write( "#!/bin/bash\n".as_bytes()){
         return Err(CompileError::io_error(error.to_string()));
     }
 
+
+
     for statement in script.statements() {
-        if let Err(error) = fs::write(&target, statement.to_bash(dry_run)){
+        if let Err(error) = file.write( statement.to_bash(dry_run).as_bytes()){
+            return Err(CompileError::io_error(error.to_string()));
+        }
+
+        if let Err(error) = file.write( "\n".as_bytes()){
             return Err(CompileError::io_error(error.to_string()));
         }
     }
